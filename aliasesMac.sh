bf42b448b229e5c737d73e89742c77ee056a5000
Caleb McCaffery <irishmac473@icloud.com>
2025-02-05 11:00:04 -0600

    Add tracked files

---
+++ aliasesMac.sh
@@ -0,0 +1,15 @@
+alias vim="nvim"
+# Homebrew
+alias bs="brew search"
+alias bi="brew install"
+alias bic="brew install --casks"
+alias bd="brew doctor"
+alias bu="brew update"
+alias bug="brew upgrade"
+
+# Editing
+# alias ev="vim $HOME/.vimrc"
+alias ez="vim $HOME/.zhrc"
+alias ea="vim $HOME/dotfiles/aliasesMac.sh"
+
+alias ua="cat $HOME/dotfiles/aliasesMac.sh"
